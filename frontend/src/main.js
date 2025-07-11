43d69a8bdf314d44c530b7aea640302d24358325
Rifky Danu Asmoro <blackrazorer@gmail.com>
2025-07-11 14:32:19 +0700

    dd

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,7 +4,7 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import animateOnScroll from './directives/animateOnScroll'; // <-- Impor
-import '@fortawesome/fontawesome-free/css/all.min.css'
+//import '@fortawesome/fontawesome-free/css/all.min.css'
 
 
 const app = createApp(App);
